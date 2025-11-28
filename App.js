077755f7decc8c9c8ac0b5aff6ddfb8b1f819eb3
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-11-27 18:00:59 -0700

    Feat: Save User credentials to firebase

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useContext } from 'react';
-import { Text, ActivityIndicator, StyleSheet} from 'react-native';
+import { useContext, useState } from 'react';
+import { Text, ActivityIndicator, StyleSheet, Button, View} from 'react-native';
 import {AuthProvider, AuthContext} from './src/contexts/AuthContext';
 
 const AuthStatus = () => {
-  const {user, profile, loading, isNewUser} = useContext(AuthContext);
+  const {user, profile, loading, isNewUser, createProfile} = useContext(AuthContext);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const handleCreate = async () => {
+    setIsCreating(true);
+    try {
+      await createProfile("Jester-Tester")
+    } catch (e) {
+      console.error("Error creating user", e);
+    }
+    setIsCreating(false);
+  }
 
   if (loading) return <ActivityIndicator size="large" color="blue" />
   return (
     <SafeAreaView>
+      <View>
       <Text style={styles.label}>Slice 2: Identity Verification</Text>
 
       <Text style={styles.label}>User ID:</Text>
@@ -19,6 +31,17 @@ const AuthStatus = () => {
 
       <Text style={styles.label}>Profile Topic:</Text>
       <Text style={styles.value}>{profile ? profile.topic : 'N/A'}</Text>
+      </View>
+
+    {isNewUser && (
+      <SafeAreaView>
+        <Button
+          title={isCreating ? "Creating..." : "Create Test Profile"}
+          onPress={handleCreate}
+          disabled={isCreating}
+        />
+      </SafeAreaView>
+    )}
     </SafeAreaView>
   );
 }
