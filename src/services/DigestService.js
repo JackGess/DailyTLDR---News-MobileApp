cf59630738515572cde702015bbd4ddecf8dd020
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-11-29 15:37:43 -0700

    Fix: Renamed the table to get the digest
    Successfully displayed digest on the screen

--- src/services/DigestService.js
+++ src/services/DigestService.js
@@ -23,16 +23,12 @@ export const triggerManualDigest = async () => {
   }
 }
 
-/**
- * Firebase Index needs to be created for this function to work
- *
- * */
 export const getLatestDigest = async (userId) => {
   try{
-    const digestRef = collection(db, "digest");
+    const digestsRef = collection(db, 'digests');
 
     const queryToGetDigest = query(
-      digestRef,
+      digestsRef,
       where('userId', '==', userId),
       orderBy('createdAt', 'desc'),
       limit(1)
@@ -40,9 +36,9 @@ export const getLatestDigest = async (userId) => {
 
     const snapshot = await getDocs(queryToGetDigest);
 
-    if(snapshot.empty) return null;
+    if (snapshot.empty) return null;
 
-    return snapshot.docs[0].data;
+    return snapshot.docs[0].data();
 
   } catch (e) {
     console.error("Failed to getLatestDigest: ", e);
